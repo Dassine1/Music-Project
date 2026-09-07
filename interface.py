4100a45883455b5acbb718129283717963162f46
Dassine1 <e2531577@cmaisonneuve.qc.ca>
2026-09-07 19:37:11 +0000

    Saving changes

--- interface.py
+++ interface.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt6.QtWidgets import QApplication, QWidget
 
+
 # 1. Initialize the application handler
 app = QApplication(sys.argv)
 
@@ -11,6 +12,10 @@ window.resize(300, 200)  # Width, Height in pixels
 
 # 3. Render the window on screen
 window.show()
+
+#  après window.show()
+window.grab().save("rendu_interface.png")  # Prend une photo de la fenêtre
+print("📸 Capture d'écran enregistrée sous 'rendu_interface.png' !")
 
 # 4. Run the main system event loop
 sys.exit(app.exec())
